3858fdbb03744f449887450fc980549dcc3932e2
Christie Pham <115462474+christie0902@users.noreply.github.com>
2024-04-30 13:05:26 +0200

    install express js and create routing and partials

--- app.js
+++ app.js
@@ -2,22 +2,38 @@ const express = require('express');
 
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
+// manually set up a different views file folder that is not views
+// app.set('views', 'myviews')
+
 app.listen(3000);
 
 
 app.get('/', (req,res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    const blogs = [
+        {title: "Blog A", snippet:'This is something something'},
+        {title: "Blog B", snippet: 'This is something something'},
+        {title: "Blog C", snippet: 'This is something something'}
+    ]
+    res.render('index', {title: "Home", blogs});
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: "About"});
 })
 
-app.get('/about-us', (req, res)=>{
-    res.redirect('/about');
+// app.get('/about-us', (req, res)=>{
+//     res.redirect('/about');
+// })
+
+app.get('/blogs/create', (req,res) =>{
+    res.render('create', {title: "Create blog"});
 })
+
 
 // 404 page
 app.use((req, res)=> {
-    res.sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', {title: "Page not found"});
 })
